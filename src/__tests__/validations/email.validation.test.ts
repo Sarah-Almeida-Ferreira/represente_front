c594f060ff024c92f55b9517d339b8aeeeb473ac
Sarah Almeida <sarahalmeida013@gmail.com>
2024-06-20 15:48:55 -0300

    :test_tube: test: Testing useInput composable

---
+++ src/__tests__/validations/email.validation.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect, assert } from 'vitest';
+import { validateEmail, emailMask } from '@/validations/email.validation.ts';
+
+describe('validateEmail', () => {
+  it('throws error - domain not specified', () => {
+    const email = 'test@email';
+
+    assert.throws(
+      () => validateEmail(email),
+      'Inclua o domínio do e-mail após "email".'
+    );
+  });
+  
+  it('doesn\'t throw error - domain specified', () => {
+    const email = 'test@email.com';
+
+    assert.doesNotThrow(
+      () => validateEmail(email),
+      'Inclua o domínio do e-mail após "email".'
+    );
+  });
+});
+
+describe('emailMask', () => {
+  it('sanitize email', () => {
+    const email = '`test-%$#~`´À@email_.c12324=+{}[]/|*ç!?:<>\';`';
+    const sanitizedEmail = emailMask(email);
+
+    expect(sanitizedEmail).toBe('test-%A@email_.c12324=+/c?:<>;');
+  });
+});
